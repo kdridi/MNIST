45c1deabef10bff0a068285663cb445c2b219959
Karim DRIDI <karim.dridi@gmail.com>
2019-02-19 11:10:36 +0100

    create java project

---
+++ src/main/java/com/arykow/mnist/App.java
@@ -0,0 +1,13 @@
+package com.arykow.mnist;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
